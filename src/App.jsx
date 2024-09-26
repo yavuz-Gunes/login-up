26b28029475526ffbd074e69427673bb50ebb3e2
yavuz-Gunes <yavuzselgunes@gmail.com>
2024-09-27 01:05:16 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import LoginSignup from './Components/LoginSignup/LoginSignup';
+
+function App() {
+  return (
+    <div className="App">
+            <LoginSignup/>
+            
+    </div>
+  );
+}
+
+export default App;
